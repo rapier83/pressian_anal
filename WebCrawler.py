30130267fad4b131aa13c971ded67797ee1dd832
rapie <rapier83@gmail.com>
2016-07-16 04:14:35 +0900

    재귀함수 호출 중. findAttr 로 bs4.element.Tag 오브젝트인 soup의 key, value를 찾는다. 만약 soup.children이 1이 아닌 경우 list화 시켜 soup의 아랫단계를 검색한다. 1이 될 때까지 내려가서 key와 value가 있는지 검사한다. 없으면 parent로 돌아간다. 1) 중첩된 리스트를 단일 리스트화 시켜서 key,value를 찾는다. 2) 재귀함수를 두개를 번갈아 가면서 이용해 key, value를 찾는다.

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
+import bs4
 from konlpy.tag import Hannanum
 from collections import Counter
 import urllib.request
@@ -9,8 +9,8 @@ import re
 class GetPage:
     def __init__(self, target):
         request = urllib.request.urlopen(target)
-        self.rawDoc = request.read()
-        self.soup = BeautifulSoup(request, 'html.parser')
+        self.rawDoc = urllib.request.urlopen(target).read()
+        self.soup = bs4.BeautifulSoup(urllib.request.urlopen(target), 'html.parser')
         self.writerList_ = list()
         # self.title = str()
         # self.date = str()
@@ -29,14 +29,14 @@ class GetPage:
     def WriterList(self):
         # Get ResultSet
         corpus = self.soup.findAll(class_ = "head_writer_fullname")
-        result = corpus[0]
 
         # Prefare substract name
         pattern = re.compile('\S*\S')
 
         # Append writers to list
-        for i in result:
-            t = pattern.findall(i.getText())[0]
+        for i in corpus.children:
+            print(i);
+            t = pattern.findall(i)
             self.writerList_.append(t)
 
         return self.writerList_
@@ -65,21 +65,36 @@ def WordCount(corpus):
     frequency = Counter(nouns)
     return frequency
 
+def findAttr(soupBody, key, value):
+    if isinstance(soupBody, bs4.element.Tag):
+        if key in soupBody.attrs.keys():
+            list_ = list(soupBody.children)
+            for i in list_:
 
+                return soupBody
+    return findAttr(soupBody.next, key, value)
+
+def findAttrForList(list_, key, value):
+
+    return findAttrForList(list_, key, value)
 
 if __name__ == '__main__':
 
     target = "http://m.pressian.com/m/m_article.html?no=138940"
+    target2 = "http://m.pressian.com/m/m_article.html?no=138936"
+    target3 = "http://m.pressian.com/m/m_article.html?no=139075"
     # target = "test.html"
 
-    k = GetPage(target)
+    k = GetPage(target2)
 
-    print(k.WriterList)
+    # print(k.WriterList)
     # k.ParagraphList
-    print(k.Article)
-    print(type(k.Article))
+    # print(k.Article)
+    # print(type(k.Article))
 
     # print(WordCount(k.soup.find(class_ = "smartOutput body_font")))
 
     #for corpus in k.ParagraphList[0:3]:
     #    print(WordCount(corpus))
+
+    findAttr(k.soup.body, 'toggle_bg')
