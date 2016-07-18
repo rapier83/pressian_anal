6205c29514fe7b07bbde1e4a90f07856705c0fed
rapie <rapier83@gmail.com>
2016-07-18 12:47:41 +0900

    재귀함수 호출을 통한 웹문서 검색은 헛수고다 `.findChildren(..., attrs = '...', ...)` 이 짱이다.

--- WebCrawler.py
+++ WebCrawler.py
@@ -19,31 +19,9 @@ class GetPage:
 
     @property
     def Article(self):
-        # doc = self.soup.findAll(class_="smartOutput body_font")
-        # startPos = doc[0].find('>')
-        # endPos = doc[0].find('</')
-        # doc = doc[0][startPos:endPos]
-        return self.soup.findAll(class_="smartOutput body_font")
+        doc = self.soup.find(class_="smartOutput body_font").getText()
 
-    @property
-    def WriterList(self):
-        # Get ResultSet
-        corpus = self.soup.findAll(class_ = "head_writer_fullname")
-
-        # Prefare substract name
-        pattern = re.compile('\S*\S')
-
-        # Append writers to list
-        for i in corpus.children:
-            print(i);
-            t = pattern.findall(i)
-            self.writerList_.append(t)
-
-        return self.writerList_
-
-    @property
-    def title(self):
-        return 0
+        return self.soup.find(class_="smartOutput body_font").getText()
 
     @property
     def ParagraphList(self):
@@ -53,11 +31,30 @@ class GetPage:
         # change to str
         # Split with \n
         # Remove \r
-        self.paragraphList_ = page.getText().split(sep='\n')
-        self.paragraphList_ = [x for x in self.paragraphList_ if x != '\r']
-        self.paragraphList_ = [x.strip() for x in self.paragraphList_]
-
+        self.paragraphList_ = page.getText().split(sep='\xa0')
+        self.paragraphList_ = [x for x in self.paragraphList_ if x != '\r' or x != '']
+        self.paragraphList_ = [x.strip('\xa0') for x in self.paragraphList_]
+        self.paragraphList_ = [x.replace('\n', '') for x in self.paragraphList_]
+        self.paragraphList_ = list(filter(('').__ne__, self.paragraphList_))
         return self.paragraphList_
+
+    @property
+    def WriterList(self):
+        # Get ResultSet
+
+        corpus = list(self.soup.findChildren(attrs = "head_writer_fullname"))
+        pattern = re.compile('\S*\S')
+        for e in corpus:
+            appendee = pattern.findall(e.getText())[0]
+            # print(appendee)
+            self.writerList_.append(appendee)
+        return self.writerList_
+
+    @property
+    def title(self):
+        return 0
+
+
 
 def WordCount(corpus):
     h = Hannanum()
@@ -65,36 +62,21 @@ def WordCount(corpus):
     frequency = Counter(nouns)
     return frequency
 
-def findAttr(soupBody, key, value):
-    if isinstance(soupBody, bs4.element.Tag):
-        if key in soupBody.attrs.keys():
-            list_ = list(soupBody.children)
-            for i in list_:
-
-                return soupBody
-    return findAttr(soupBody.next, key, value)
-
-def findAttrForList(list_, key, value):
-
-    return findAttrForList(list_, key, value)
-
 if __name__ == '__main__':
 
     target = "http://m.pressian.com/m/m_article.html?no=138940"
     target2 = "http://m.pressian.com/m/m_article.html?no=138936"
     target3 = "http://m.pressian.com/m/m_article.html?no=139075"
-    # target = "test.html"
+    targetFile = "test.html"
 
     k = GetPage(target2)
 
-    # print(k.WriterList)
-    # k.ParagraphList
-    # print(k.Article)
-    # print(type(k.Article))
+    # print(k.ParagraphList)
+    print(k.Article)
+    print(type(k.Article))
+    print(WordCount(k.Article))
 
     # print(WordCount(k.soup.find(class_ = "smartOutput body_font")))
 
-    #for corpus in k.ParagraphList[0:3]:
+    #for corpus in k.ParagraphList:
     #    print(WordCount(corpus))
-
-    findAttr(k.soup.body, 'toggle_bg')
